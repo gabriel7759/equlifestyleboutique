ad6a1e8de83c69565e0feea7d0d68228608d6791
gvelazquez <gvelazquez@abargon.com>
2015-11-12 19:44:12 -0600

    sending emails

--- application/classes/controller/api.php
+++ application/classes/controller/api.php
@@ -43,8 +43,34 @@ class Controller_Api extends Controller {
 			// Send to seller
 			$register = Model::factory('registration')->insert($data);
 
-			if($register)
+			if($register) {
+				
+				$subject = utf8_encode('Equ Lifestyle Boutique');
+				$message = '
+					Hello!
+					<br /><br />
+					First of all, we would like to THANK YOU for signing up and becoming part of the EQU family! We are so excited to be able to launch EQU Online and provide you with an easy way to connect to the brands you love any time of day. 
+					<br /><br />	
+					By signing up to EQU Online you will be the first to know about new products, sales, promotions and contests both online and in store. We will be offering a broader range of products and brands online, we can’t wait to show you!
+					<br /><br />	
+					We promise not to send you more than one email a week, you can unsubscribe anytime.
+					<br /><br />
+					Welcome to the Family!
+					<br /><br />
+				';
+				$email = Email::factory($subject, $message, 'text/html')
+				->from('no-reply@equlifestyleboutique.com', 'Equ Lifestyle Boutique');
+				$send = FALSE;
+				$email->to($data['email'], $subject);		
+				$send = TRUE;
+				
+				if ($send)
+				{
+					$email->send();
+				}
+
 				$result = array("success"=>TRUE, "error"=>FALSE, "errordesc"=>"");
+			}
 		}
 		
 		$response = json_encode($result);
